0210c5a2e9b7b205029511802c50f5deebe35e3a
aimms321 <aimms321@163.com>
2017-10-02 10:54:23 +0800

    generated and modified the mappers and pojo 2

---
+++ src/main/java/com/happymmall/dao/OrderMapper.java
@@ -0,0 +1,17 @@
+package com.happymmall.dao;
+
+import com.happymmall.pojo.Order;
+
+public interface OrderMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    Order selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
