cb165ffa17f6098ad316108dacefc6fe13b502c5
aimms321 <aimms321@163.com>
2017-10-10 00:27:22 +0800

    generated file in common

---
+++ src/main/java/com/happymmall/controller/portal/UserController.java
@@ -0,0 +1,26 @@
+package com.happymmall.controller.portal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/user/")
+public class UserController {
+    /**
+     * 用户登录
+     * @param username
+     * @param password
+     * @param httpsession
+     * @return
+     */
+    @RequestMapping(value = "login.do",method = RequestMethod.POST)
+    @ResponseBody
+    public Object login(String username, String password, HttpSession httpsession) {
+        return null;
+    }
+}
---
+++ src/main/java/com/happymmall/service/impl/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.happymmall.service.impl;
+
+import com.happymmall.service.IUserService;
+
+public class UserServiceImpl implements IUserService {
+
+
+    public Object login(String username, String password) {
+        return null;
+    }
+}
